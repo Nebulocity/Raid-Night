d7254d377c7de69a2e3664e986d430727573bc47
Nebulocity <Nebulocity@users.noreply.github.com>
2026-03-18 13:04:23 -0700

    Update RaidSelectScene.js

--- scenes/RaidSelectScene.js
+++ scenes/RaidSelectScene.js
@@ -121,7 +121,8 @@ export default class RaidSelectScene extends Phaser.Scene {
         yoyo: true,
         ease: 'Quad.easeOut',
         onComplete: () => flash.destroy()
-
+      }
+                      
       // Camera fades out
       this.cameras.main.fadeOut(1000, 0, 0, 0);
       this.time.delayedCall(500, () => {
