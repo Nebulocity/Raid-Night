30410e3c13ba379ceb216f80c07e69715a3999f0
Nebulocity <Nebulocity@users.noreply.github.com>
2026-03-23 12:08:01 -0700

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -4347,7 +4347,7 @@ export default class GameScene extends Phaser.Scene {
 
     const saveData     = loadSaveData();
     const selectedRaidId = this.registry.get('selectedRaidId') || 'spookspire_keep';
-    const selectedBossId = this.registry.get('selectedBossId') || 'sir_trotsalot_and_nighttime';
+    const selectedBossId = this.registry.get('selectedBossId') || 'sir_trotsalot';
 
     const updatedSave = recordBossDefeat(saveData, selectedRaidId, selectedBossId);
     this.registry.set('saveData', updatedSave);
